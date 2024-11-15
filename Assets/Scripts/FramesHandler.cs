bed257d396a596a7df70c19bb713871d48f03f10
Maximilian Kolomainen <111704018+ShivadeT0T@users.noreply.github.com>
2024-11-15 14:11:01 +0900

    Added new classes
    FramesHandler.cs and SaveLoad.cs added. Some stuff added to FramesHandler.cs

---
+++ Assets/Scripts/FramesHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+public enum HandlerType
+{
+    SAVE,
+    LOAD,
+    BOTH
+}
+public class FramesHandler
+{
+    private readonly int FrameLimit;
+    private int FrameCount = 0;
+    public BackgroundDataNoDepth[] FramesArray;
+    public ConcurrentQueue<BackgroundDataNoDepth> FramesProcessor;
+    public List<BackgroundDataNoDepth> FramesList;
+
+    public FramesHandler(int frameLimit, HandlerType handlerType)
+    {
+        FrameLimit = frameLimit;
+
+        switch (handlerType)
+        {
+            case HandlerType.SAVE:
+                FramesArray = new BackgroundDataNoDepth[FrameLimit];
+                FramesProcessor = new ConcurrentQueue<BackgroundDataNoDepth>();
+                break;
+            case HandlerType.LOAD:
+                FramesList = new List<BackgroundDataNoDepth>();
+                break;
+            case HandlerType.BOTH:
+                FramesArray = new BackgroundDataNoDepth[FrameLimit];
+                FramesProcessor = new ConcurrentQueue<BackgroundDataNoDepth>();
+                FramesList = new List<BackgroundDataNoDepth>();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ProcessFrame()
+    {
+
+    }
+
+}
